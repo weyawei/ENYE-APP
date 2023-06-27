1fcf5dba6cc1c039a6d9c018d5651cdbbf21f189
Ron <ronfrancia.enye@gmail.com>
2023-06-27 17:06:46 +0800

    Product Page updated2x

--- lib/widget/carousel_card.dart
+++ lib/widget/carousel_card.dart
@@ -5,29 +5,18 @@ import 'package:flutter/material.dart';
 
 import '../screens/products/category_model.dart';
 
-class CarouselCard extends StatefulWidget {
+class CarouselCard extends StatelessWidget {
   final Category1 category;
   const CarouselCard({required this.category,});
 
   @override
-  State<CarouselCard> createState() => _CarouselCardState();
-}
-
-class _CarouselCardState extends State<CarouselCard> {
-  @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: (){
-        setState(() {
-          Navigator.of(context, rootNavigator: true).pushNamed(
-            CatalogScreen.routeName,
-            arguments: widget.category,
-          );
-        });
-        /*Navigator.pushNamed(context,
+        Navigator.pushNamed(context,
           CatalogScreen.routeName,
-          arguments: widget.category,
-        );*/
+          arguments: category,
+        );
       },
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 20),
@@ -35,7 +24,7 @@ class _CarouselCardState extends State<CarouselCard> {
           borderRadius: BorderRadius.all(Radius.circular(5.0)),
           child: Stack(
             children: <Widget>[
-              Image.asset(widget.category.imageUrl, fit: BoxFit.cover, width: 1000.0),
+              Image.asset(category.imageUrl, fit: BoxFit.cover, width: 1000.0),
               Positioned(
                 bottom: 0.0,
                 left: 0.0,
@@ -54,7 +43,7 @@ class _CarouselCardState extends State<CarouselCard> {
                   padding:
                   EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                   child: Text(
-                    widget.category.name,
+                    category.name,
                     style: Theme.of(context).textTheme.headlineMedium?.copyWith(
                       color: Colors.white,
 
