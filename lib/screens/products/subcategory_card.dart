188de8e9b3cc2e6da60274893bc9621a9a4cda1c
Ron <ronfrancia.enye@gmail.com>
2023-07-05 16:59:53 +0800

    subcategory edit new UI , tinanggal na yung Picture

--- lib/screens/products/subcategory_card.dart
+++ lib/screens/products/subcategory_card.dart
@@ -37,79 +37,68 @@ class SubCategoryCard extends StatelessWidget {
               children: [
                 Container(
                   width: MediaQuery.of(context).size.width / widthFactor,
-                  height: 200.0,
+                  height: 80.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+
+                  ),
 
                 ),
-                Positioned(
-                  top: 60,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 112,
-                    decoration: BoxDecoration(
-                      color: Colors.black.withAlpha(50),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 65,
-                  left: 5,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width / widthFactor -10,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: SingleChildScrollView(
-                        child: Row(
-                          children: [
-                            Expanded(
-                              flex: 2,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    subcategory.subcategory,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall!
-                                        .copyWith(color: Colors.white),
-                                  ),
-                                ],
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SingleChildScrollView(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 10,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                subcategory.subcategory,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headlineSmall!
+                                    .copyWith(color: Colors.black),
                               ),
-                            ),
-                            Expanded(
-                              child: IconButton(
-                                icon: Icon(
-                                  Icons.arrow_forward,
-                                  color: Colors.white,
-                                  size: 40,
-                                ),
-                                onPressed: () {
-                                  PersistentNavBarNavigator.pushNewScreenWithRouteSettings(
-                                    context,
-                                    settings: RouteSettings(
-                                      name: CatalogScreen.routeName,
-                                      arguments: {
-                                        'subcategory': subcategory.subcategory
-                                      },
-                                    ),
-                                    screen: CatalogScreen(),
-                                    withNavBar: true,
-                                    pageTransitionAnimation:
-                                    PageTransitionAnimation.cupertino,
-                                  );
-                                },
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
+                        Expanded(
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                            onPressed: () {
+                              PersistentNavBarNavigator.pushNewScreenWithRouteSettings(
+                                context,
+                                settings: RouteSettings(
+                                  name: CatalogScreen.routeName,
+                                  arguments: {
+                                    'subcategory': subcategory.subcategory
+                                  },
+                                ),
+                                screen: CatalogScreen(),
+                                withNavBar: true,
+                                pageTransitionAnimation:
+                                PageTransitionAnimation.cupertino,
+                              );
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
+
               ],
             ),
 
