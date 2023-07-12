8ee5ee8c5de41218cd8c4cccc795e21aa0e2da96
weyawei <eule.weajinelette@gmail.com>
2023-07-12 15:32:17 +0800

    Projects Has Catalog

--- lib/screens/projects/detailed_projects.dart
+++ lib/screens/projects/detailed_projects.dart
@@ -1,33 +1,82 @@
-import 'package:enye_app/widget/widgets.dart';
-import 'package:flutter/material.dart';
-import 'package:enye_app/screens/screens.dart';
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:open_file_plus/open_file_plus.dart';
+import 'package:path_provider/path_provider.dart';
 
-class detailedProjPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../../config/api_connection.dart';
+import '../../widget/custom_appbar.dart';
+import '../screens.dart';
+
+class detailedProjPage extends StatefulWidget {
   static const String routeName = '/detailedproj';
 
-  static Route route(){
+  Route route(){
     return MaterialPageRoute(
       settings: RouteSettings(name: routeName),
-      builder: (context) => detailedProjPage(),
+      builder: (context) => detailedProjPage(projects: projects,),
     );
+  }
+
+  final Projects projects;
+  detailedProjPage({required this.projects});
+
+  @override
+  State<detailedProjPage> createState() => _detailedProjPageState();
+}
+
+class _detailedProjPageState extends State<detailedProjPage> {
+
+  double? _progress;
+  Future openFile ({required String url, String? filename}) async {
+    final file = await downloadFile(url, filename!);
+
+    if (file == null) return;
+
+    print('Path: ${file.path}');
+    OpenFile.open(file.path);
+  }
+
+  Future <File?> downloadFile (String url, String name) async {
+    final appStorage = await getApplicationDocumentsDirectory();
+    final file = File('${appStorage.path}/$name');
+
+    final response = await Dio().get(
+        url,
+        options: Options(
+          responseType: ResponseType.bytes,
+          followRedirects: false,
+          //receiveTimeout: 0
+        ),
+        onReceiveProgress: (recivedBytes, totalBytes) {
+          setState(() {
+            _progress = recivedBytes / totalBytes;
+            if (_progress == 1.0){
+              _progress = null;
+            }
+          });
+          print(_progress);
+        }
+    );
+
+    final raf = file.openSync(mode: FileMode.write);
+    raf.writeFromSync(response.data);
+    await raf.close();
+
+    return file;
+
   }
 
   @override
   Widget build(BuildContext context) {
-
-    final filteredProjects = ModalRoute.of(context)?.settings?.arguments as Map<String, String>;
-    final projId = filteredProjects['projId'].toString();
-
-    List<Projects> detailedProjects = [];
-
-    detailedProjects = projectList.where((projectList) => projectList.proj_id == projId).toList();
 
     return Scaffold(
       extendBody: true,
       appBar: CustomAppBar(title: 'ENYE CONTROLS', imagePath: 'assets/logo/enyecontrols.png',),
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(image: NetworkImage("${detailedProjects[0].images}"), fit: BoxFit.cover),
+          image: DecorationImage(image: NetworkImage("${API.projectsImage + widget.projects.images}"), fit: BoxFit.fill),
         ),
         child: Container(
           decoration: BoxDecoration(
@@ -54,7 +103,7 @@ class detailedProjPage extends StatelessWidget {
                     ),
                   ),
                   child: Text(
-                    "${detailedProjects[0].title}",
+                    "${widget.projects.title}",
                     style: TextStyle(fontSize: 32.0, color: Colors.white, fontWeight: FontWeight.bold),
                     textAlign: TextAlign.right,
                   ),
@@ -75,7 +124,7 @@ class detailedProjPage extends StatelessWidget {
                     ),
                   ),
                   child: Text(
-                    "${detailedProjects[0].description1}",
+                    "${widget.projects.description1}",
                     style: TextStyle(
                       fontSize: 18.0,
                       color: Colors.white,
@@ -105,7 +154,7 @@ class detailedProjPage extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.only(top: 20),
                     child: Text(
-                      "${detailedProjects[0].description2}",
+                      "${widget.projects.description2}",
                       style: TextStyle(
                         fontSize: 15.0,
                         color: Colors.white,
@@ -114,6 +163,49 @@ class detailedProjPage extends StatelessWidget {
                         letterSpacing: 0.5,
                       ),
                       textAlign: TextAlign.right,
+                    ),
+                  ),
+                ),
+              ),
+
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 1,
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.centerLeft,
+                      colors: [Colors.deepOrange.shade300, Colors.deepOrange.withOpacity(0.1)],
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20, left: 60),
+                    child:
+                    widget.projects.projCatalogs == null ||  widget.projects.projCatalogs.isEmpty
+                        ? Container()
+                        : Row(
+                      children: [
+                        Lottie.network("https://assets3.lottiefiles.com/packages/lf20_Sz5T65.json", height: 50,),
+                        _progress != null
+                            ? const CircularProgressIndicator()
+                            :TextButton(
+                          child: Text("View Catalog",
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                wordSpacing: 2.0,
+                                letterSpacing: 0.5,
+                                decoration: TextDecoration.underline
+                            ),),
+                          onPressed: () => openFile(
+                            url: "${API.fileCatalogsPdf + widget.projects.projCatalogs}",
+                            filename: "${widget.projects.projCatalogs}",
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
---
+++ lib/screens/projects/projectsvc.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../../../config/api_connection.dart';
+import '../screens.dart';
+
+class projectSVC {
+  //this is same as in PHP code action made by the user CRUD
+  static const GET_ALL_PROJCATEGORY = 'get_all_projcat';
+  static const GET_ALL_PROJECTS = 'get_all_projects';
+
+
+  //get data categories from database
+  static Future <List<projCategories>> getProjCategory() async {
+    try{
+      var map = Map<String, dynamic>();
+      map['action'] = GET_ALL_PROJCATEGORY;
+
+      //get all data of categories
+      final res = await http.post(Uri.parse(API.projects), body: map); //passing value to result
+      print('getProjCategory Response: ${res.body}');
+
+      if(res.statusCode == 200){
+        List<projCategories> list = parseResponseProjCateg(res.body);
+        return list;
+      } else {
+        throw Exception('Failed to retrieve Project Categories');
+        //return List<Categories>();
+      }
+    } catch (e) {
+      throw Exception('Failed to retrieve Project Categories');
+      //return List<Categories>();
+    }
+  }
+
+  static List<projCategories> parseResponseProjCateg(String responseBody){
+    //conversion from web server into data by using categories.dart
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<projCategories>((json) => projCategories.fromJson(json)).toList();
+  }
+
+  static Future <List<Projects>> getProjects() async {
+    var map = Map<String, dynamic>();
+    map['action'] = GET_ALL_PROJECTS;
+
+    //get all data of categories
+    final res = await http.post(Uri.parse(API.projects), body: map); //passing value to result
+    print('getProjects Response: ${res.body}');
+
+    if(res.statusCode == 200){
+      List<Projects> list = parseResponse(res.body);
+      return list;
+    } else {
+      throw Exception('Failed to retrieve Project');
+      //return List<Categories>();
+    }
+  }
+
+  static List<Projects> parseResponse(String responseBody){
+    //conversion from web server into data by using categories.dart
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<Projects>((json) => Projects.fromJson(json)).toList();
+  }
+
+}
