1fedc87747aeca315d7fdaecded844b426659961
weyawei <eule.weajinelette@gmail.com>
2023-07-13 11:30:12 +0800

    SYSTEM TECH SPECS DATA

--- lib/screens/systems/systemsvc.dart
+++ lib/screens/systems/systemsvc.dart
@@ -7,6 +7,7 @@ import '../screens.dart';
 class systemService {
   //this is same as in PHP code action made by the user CRUD
   static const GET_ALL_SYSTEMS = 'get_all_systems';
+  static const GET_ALL_SYSDETAILS = 'get_all_sysdetails';
   static const GET_ALL_SYSTECHSPECS = 'get_all_systechspecs';
 
   //get data categories from database
@@ -36,6 +37,31 @@ class systemService {
     //conversion from web server into data by using categories.dart
     final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
     return parsed.map<Systems>((json) => Systems.fromJson(json)).toList();
+  }
+
+  static Future <List<SystemsDetail>> getSysDetails(String system_id) async {
+
+      var map = Map<String, dynamic>();
+      map['action'] = GET_ALL_SYSDETAILS;
+      map['id'] = system_id;
+
+      //get all data of categories
+      final res = await http.post(Uri.parse(API.systems), body: map); //passing value to result
+      print('getSysTechSpecs Response: ${res.body}');
+
+      if(res.statusCode == 200){
+        List<SystemsDetail> list = parseResSysDetails(res.body);
+        return list;
+      } else {
+        throw Exception('Failed to retrieve categories');
+      }
+
+  }
+
+  static List<SystemsDetail> parseResSysDetails(String responseBody){
+    //conversion from web server into data by using categories.dart
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<SystemsDetail>((json) => SystemsDetail.fromJson(json)).toList();
   }
 
   static Future <List<SystemsTechSpecs>> getSysTechSpecs(String system_id) async {
