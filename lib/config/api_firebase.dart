147dc9fa21467fe37ff3a229b090693623396a86
Ron <ronfrancia.enye@gmail.com>
2023-08-07 13:45:37 +0800

    Push notif even if the apps is open DONE

--- lib/config/api_firebase.dart
+++ lib/config/api_firebase.dart
@@ -1,9 +1,13 @@
 
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_session_manager/flutter_session_manager.dart';
 
 import '../main.dart';
 
+/*
 Future<void> handleBackgroundMessage(RemoteMessage message) async{
   print('Title: ${message.notification?.title}');
   print('Body: ${message.notification?.body}');
@@ -34,6 +38,80 @@ class FirebaseApi{
 
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
   }
+*/
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async{
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
+
+
+class FirebaseApi{
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notification',
+    importance: Importance.defaultImportance,
+  );
+  final _localNotifications = FlutterLocalNotificationsPlugin();
+
+  void handleMessage(RemoteMessage? message){
+    if (message==null) return;
+
+    navigatorKey.currentState?.pushReplacementNamed('/', arguments: message);
+    //  NotificationScreen.route,
+    //  arguments: message,
+    //  );
+  }
+  Future initLocalNotifications() async{
+    //  const iOS = IOSInitializationSettings();
+    const android = AndroidInitializationSettings('drawable/enye');
+    const settings =  InitializationSettings(android: android);
+
+    await _localNotifications.initialize(
+        settings,
+        onDidReceiveNotificationResponse: (payload){
+          final message = RemoteMessage.fromMap(jsonDecode(payload.payload!));
+          handleMessage(message);
+        }
+    );
+
+    final platform = _localNotifications.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
+  }
+
+  Future initPushNotification() async{
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if(notification==null) return;
+
+      _localNotifications.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            _androidChannel.id,
+            _androidChannel.name,
+            channelDescription: _androidChannel.description,
+            icon: '@drawable/enye',
+          ),
+        ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
+  }
 
 
   Future<void> initNotifications() async{
@@ -42,6 +120,7 @@ class FirebaseApi{
     await SessionManager().set("token", fCMToken);
     print('Token: $fCMToken');
     initPushNotification();
+    initLocalNotifications();
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 }
